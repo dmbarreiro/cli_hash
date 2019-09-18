0549e21a6cc3d7c33d0cd5be32ece30ab9ba6ca6
dmbarreiro <5133990+dmbarreiro@users.noreply.github.com>
2019-09-18 11:15:16 +0200

    Adding hash comparisson. Changing program interface to add a switch parameter accepting "generate" and "compare" as values to determine the action to be performed and reading strings and hashes from files instead of arguments (this makes easier to include problematic caracters like $ in the strings). Updating README to include all these changes and new instructions.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CLI Hash Generator
 
-Generates a salted hash from the string provided as argument using the provided number of rounds to generate the hash salt.
+Generates a salted hash from input file using selected number of rounds to generate the hash salt. If another input file containing a hash is provided it will check if the provided hash matches the other input string.
 
 #### Install
 
@@ -9,16 +9,44 @@ CLI Hash Generator is an npm project therefore to install it you just have to en
 
 #### Execute:
 
-```
-npm start -- <string-to-hash> <salt-generation-rounds>
-```
-
-Example:
+To hash:
 
 ```
-npm start -- test 12
+npm start -- generate <path-to-file> <salt-generation-rounds>
 ```
 
-Hashes string `test` using `12` rounds to generate the hashing salt.
+To compare:
+
+```
+npm start -- compare <path-to-file> <salt-generation-rounds> <path-to-file-containing-hash>
+```
+
+#### Examples
+
+The repository contains test files under `test/` that you can use to check the behavior.
+
+##### 1. Generate hash from string
+
+```
+npm start -- generate test/string-to-hash.txt 12
+```
+
+Hashes string contained in `test/string-to-hash.txt` using `12` rounds to generate the hashing salt. The generated hash will be printed in the console.
+
+##### 2. Compare string to hash
+
+```
+npm start -- compare test/string-to-hash.txt test/hash-to-check.txt
+```
+
+Checks string contained in `test/string-to-hash.txt` matches hash contained in `test/hash-to-check.txt`. The hashes should match and `✔ String and hash match` will be printed in the console.
+
+Check for negative comparison with
+
+```
+npm start -- compare test/wrong-string-to-hash.txt test/hash-to-check.txt
+```
+
+`✘ String and hash do NOT match` will be printed in the console.
 
 CLI Hash Generator uses [bcryptjs](https://www.npmjs.com/package/bcryptjs).
--- app.mjs
+++ app.mjs
@@ -1,23 +1,46 @@
 import bcryptjs from "bcryptjs";
+import bluebird from "bluebird";
+import fsCallback from "fs";
+const fs = bluebird.promisifyAll(fsCallback);
 
-if (process.argv.length !== 4) {
-    console.log('The program acceps to arguments:')
-    console.log('+ The string to hash');
-    console.log('+ Number of rounds used to generate the hashing salt');
+const fileStringToHash = process.argv[3];
+
+if (process.argv[2] === "generate") {
+    const saltRounds = Number(process.argv[4]);
+    if (Number.isNaN(saltRounds)) {
+        console.log(`Salt rounds must be a number`);
+        process.exit(1);
+    }
+    fs.readFileAsync(fileStringToHash, "utf8")
+        .then((stringToHash) => {
+            return bcryptjs.hash(stringToHash, saltRounds);
+        }).then((result) => {
+            console.log(result);
+        }).catch((err) => {
+            console.log(`Error: ${err}`);
+        });
+} else if (process.argv[2] === "compare") {
+    const fileHashToCheck = process.argv[4];
+    const readStringToHash = fs.readFileAsync(fileStringToHash, "utf8");
+    const readHash = fs.readFileAsync(fileHashToCheck, "utf8");
+    Promise.all([readStringToHash, readHash])
+        .then((resultsRead) => {
+            const stringToHash = resultsRead[0];
+            const hashToCheck = resultsRead[1];
+            return bcryptjs.compare(stringToHash, hashToCheck);
+        }).then((result) => {
+            if (result) {
+                console.log("✔ String and hash match");
+            } else {
+                console.log("✘ String and hash do NOT match");
+            }
+        }).catch((err) => {
+            console.log(`Error: ${err}`);
+        });
+} else {
+    console.log("The program acceps three arguments:");
+    console.log("+ Action to perform (generate or compare hashes)");
+    console.log("+ The string to hash");
+    console.log("+ Number of rounds used to generate the hashing salt");
     process.exit(1);
 }
-
-const stringToHash = process.argv[2];
-const saltRounds = Number(process.argv[3]);
-
-if (Number.isNaN(saltRounds)) {
-    console.log(`Salt rounds must be a number`);
-    process.exit(1);
-}
-bcryptjs.hash(stringToHash, saltRounds)
-    .then((hash) => {
-        console.log(hash);
-    })
-    .catch((err) => {
-        console.log(`Error: ${err}`);
-    });
