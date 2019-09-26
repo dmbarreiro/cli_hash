eaaecabae7d161352de6d18109764ea300da65ae
David Martinez Barreiro <5133990+dmbarreiro@users.noreply.github.com>
2019-09-26 12:51:20 +0200

    Dev (#6)
    * Adding editor config
    
    * Adding eslint and git hooks to lint before commit and push

--- bin/cli_hash.mjs
+++ bin/cli_hash.mjs
@@ -1,20 +1,20 @@
 #! /usr/bin/env node --experimental-modules --no-warnings
 
-import yargs from 'yargs';
+import yargs from "yargs";
 
-import { cli_hash_generate, cli_hash_compare } from '../app.mjs';
+import { cli_hash_generate, cli_hash_compare } from "../app.mjs";
 
-const argv = yargs
-    .command('generate', 'Generates bcryptjs hash from file content using salt', (yargs) => {
-        return yargs.option('s', {
-            alias: 'salt',
+yargs
+    .command("generate", "Generates bcryptjs hash from file content using salt", (yargs) => {
+        return yargs.option("s", {
+            alias: "salt",
             default: 12,
-            describe: 'Salt length',
-            type: 'number'
-        }).option('o', {
-            alias: 'output',
-            describe: 'Output file',
-            type: 'string'
+            describe: "Salt length",
+            type: "number"
+        }).option("o", {
+            alias: "output",
+            describe: "Output file",
+            type: "string"
         });
     }, (argv) => {
         if (argv.output) {
@@ -23,27 +23,27 @@ const argv = yargs
             cli_hash_generate(argv.input_file, argv.salt);
         }
     })
-    .command('compare', 'Compares file content with hash contained in file', (yargs) => {
-        return yargs.option('c', {
-            alias: 'hash_file',
-            describe: 'Hash file to load',
+    .command("compare", "Compares file content with hash contained in file", (yargs) => {
+        return yargs.option("c", {
+            alias: "hash_file",
+            describe: "Hash file to load",
             demandOption: true,
-            type: 'string'
+            type: "string"
         });
     }, (argv) => {
         cli_hash_compare(argv.input_file, argv.hash_file);
     })
-    .example('$0 generate -i my_file.txt -s 10', 'Generates hash from content of my_file.txt a salt of length 10')
-    .example('$0 compare -i my_file.txt -c hash_file.txt', 'Compares hash of my_file.txt contents with hash contained in hash_file.txt')
-    .option('i', {
-        alias: 'input_file',
+    .example("$0 generate -i my_file.txt -s 10", "Generates hash from content of my_file.txt a salt of length 10")
+    .example("$0 compare -i my_file.txt -c hash_file.txt", "Compares hash of my_file.txt contents with hash contained in hash_file.txt")
+    .option("i", {
+        alias: "input_file",
         demandOption: true,
-        describe: 'File to load',
-        type: 'string',
+        describe: "File to load",
+        type: "string",
         nargs: 1
     })
-    .help('h')
-    .alias('h', 'help')
+    .help("h")
+    .alias("h", "help")
     .version()
-    .alias('v', 'version')
-    .argv
+    .alias("v", "version")
+    .argv;
